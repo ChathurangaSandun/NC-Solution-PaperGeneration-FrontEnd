7285188e1463ca51710af11a4d6cf4c8f4345ca8
Chathuranga Sandun <chathurangasandunkumara@gmail.com>
2018-04-30 15:48:23 +0530

    algorithms hardcorded to table

--- src/app/main/content/admin/algorithm/algorithm.component.ts
+++ src/app/main/content/admin/algorithm/algorithm.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MatPaginator, MatSort } from '@angular/material';
+import {MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
 import { DataSource } from '@angular/cdk/collections';
 
 import 'rxjs/add/operator/startWith';
@@ -24,8 +24,11 @@ import { FuseUtils } from '@fuse/utils';
 })
 export class AlgorithmComponent implements OnInit {
 
-  algorithms = [{ 'id': 1}, { 'id': 2}];
-  displayedColumns = ['id', 'name', 'description']
+  // displayedColumns = ['id', 'name', 'description'];
+  // dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  displayedColumns = ['id', 'name', 'description'];
+     dataSource = new MatTableDataSource(ELEMENT_DATA);
 
   constructor() { }
 
@@ -33,3 +36,33 @@ export class AlgorithmComponent implements OnInit {
   }
 
 }
+// export interface Algorithm {
+//   id: number;
+//   name: string;
+//   description: string;  
+// }
+
+// const ELEMENT_DATA: Algorithm[] = [
+//   {id: 1, name: 'aaaaa', description: 'aaa'},
+//   {id: 2, name: 'bbbbb', description: 'bbb'},
+//   {id: 3, name: 'ccccc', description: 'ccc'},
+  
+// ];
+
+export interface Element {
+  name: string;
+  id: number;
+  description: string;
+}
+
+const ELEMENT_DATA: Element[] = [
+  {id: 1, name: 'Hydrogen',  description: 'H'},
+  {id: 2, name: 'Helium',  description: 'He'},
+  {id: 3, name: 'Lithium',  description: 'Li'},
+  {id: 4, name: 'Beryllium',  description: 'Be'},
+  {id: 5, name: 'Boron',  description: 'B'},
+  {id: 6, name: 'Carbon', description: 'C'},
+ 
+];
+
+
